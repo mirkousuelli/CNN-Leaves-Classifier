965114408dab1df9d7511d2115313c61ae4f4de4
Mirko Usuelli <mirko.usuelli@mail.polimi.it>
2021-11-13 14:51:07 +0100

    add files

---
+++ ToySubmission/model.py
@@ -0,0 +1,15 @@
+import os
+import tensorflow as tf
+
+class model:
+    def __init__(self, path):
+        self.model = tf.keras.models.load_model(os.path.join(path, 'SubmissionModel'))
+
+    def predict(self, X):
+        
+        # Insert your preprocessing here
+
+        out = self.model.predict(X)
+        out = tf.argmax(out, axis=-1)
+
+        return out
